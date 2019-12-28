e24bea279e8af4e5b07ba5024bba8cd3317e4e38
Hagnas3 <59312340+Hagnas3@users.noreply.github.com>
2019-12-28 18:15:42 +0200

    Create search.php

---
+++ search.php
@@ -0,0 +1,35 @@
+<?php
+  include 'dbh.php';
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Seaarch page</title>
+  </head>
+  <body>
+    <h1>Search page</h1>
+
+    <div class="product-container">
+      <?php
+        if (isset($_POST['submit-search'])) {
+          $search = mysqli_real_escape_string($conn, $_POST['search']);
+          $sql = "SELECT * FROM product_info WHERE p_title LIKE '%$search%' OR p_description LIKE '%$search%'";
+          $result = mysqli_query($conn, $sql);
+          $queryResult = mysqli_num_rows($result);
+
+          if ($queryResult > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+              echo "<div>
+                  <h3>".$row['p_title']."</h3>
+                  <p>".$row['p_description']."</p>
+              </div>";
+            }
+          } else {
+            echo "There are no results matching your search!";
+          }
+        }
+      ?>
+    </div>
+  </body>
+</html>
