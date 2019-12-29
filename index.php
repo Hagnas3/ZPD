61bfc16424f7d491ec3b44bc6f252dc4acccd9c0
Hagnas3 <59312340+Hagnas3@users.noreply.github.com>
2019-12-29 17:33:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,34 +3,50 @@
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="tex/css" href="style.css">
-    <title></title>
-  </head>
+<head>
+  <meta charset="utf-8">
+  <script src="https://kit.fontawesome.com/0c5de2e8ba.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="slider.css">
+  <title>ZPD</title>
+</head>
   <body>
-    <form action="search.php" method="post">
-      <input type="text" name="search" placeholder="Search">
-      <button type="submit" name="submit-search">Search</button>
-    </form>
-    <h1>Front page</h1>
-    <h2>all products:</h2>
+    <section class="navbar">
+      <div class="max">
+        <ul class="items">
+          <li class="title"><h1>PAGE TITLE</h1></li>
+          <li class="btn1"><a href="#">HOME</a></li>
+          <li class="btn2"><a>ABOUT US</a></li>
+          <li class="search">
+            <form class="search-insides" action="search.php" method="post">
+              <input type="text" name="search" placeholder="">
+              <i class="fas fa-search"><h4>Search</h4></i>
+              <button class="search-btn" type="submit" name="submit-search">Enter</button>
+            </form>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <!-- <section class="banner">
 
+    </section> -->
     <div class="product-container">
       <?php
-        $sql = "SELECT * FROM products";
-        $result = mysqli_query($conn, $sql);
-        $queryResults = mysqli_num_rows($result);
-
-        if ($queryResults > 0) {
-          while ($row = mysqli_fetch_assoc($result)) {
-            echo "<div>
-                <img src=".$row['img_dir'].">
-                <h3>".$row['Title']."</h3>
-                <p>".$row['Description']."</p>
-            </div>";
-          }
-        }
+        // $sql = "SELECT * FROM products";
+        // $result = mysqli_query($conn, $sql);
+        // $queryResults = mysqli_num_rows($result);
+        //
+        // if ($queryResults > 0) {
+        //   while ($row = mysqli_fetch_assoc($result)) {
+        //     echo "<div>
+        //         <img width='500' src=".$row['img_dir'].">
+        //         <h3>".$row['Title']."</h3>
+        //         <p>".$row['Description']."</p>
+        //     </div>";
+        //   }
+        // }
       ?>
     </div>
   </body>
