a45331507980e7346f63af056fc0f15f5fde7e80
Hagnas3 <59312340+Hagnas3@users.noreply.github.com>
2019-12-29 14:36:26 +0200

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,8 +1,9 @@
 <?php
-$server = "localhost";
-$username = "root";
-$password = "toor";
-$dbname = "products";
+//mysql://b39dbb0c52164b:67fcf717@eu-cdbr-west-02.cleardb.net/heroku_91980633ea4f7b1?reconnect=true
+$server = "eu-cdbr-west-02.cleardb.net";
+$username = "b39dbb0c52164b";
+$password = "67fcf717";
+$dbname = "heroku_91980633ea4f7b1";
 
 $conn = mysqli_connect($server, $username, $password, $dbname);
 
