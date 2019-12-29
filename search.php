e14c58f5383504346c3b69402b9a43fd12b3932e
Hagnas3 <59312340+Hagnas3@users.noreply.github.com>
2019-12-29 17:33:35 +0200

    Update search.php

--- search.php
+++ search.php
@@ -12,24 +12,24 @@
 
     <div class="product-container">
       <?php
-        if (isset($_POST['submit-search'])) {
-          $search = mysqli_real_escape_string($conn, $_POST['search']);
-          $sql = "SELECT * FROM products WHERE Title LIKE '%$search%' OR Description LIKE '%$search%'";
-          $result = mysqli_query($conn, $sql);
-          $queryResult = mysqli_num_rows($result);
-
-          if ($queryResult > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-              echo "<div>
-                  <img src=".$row['img_dir'].">
-                  <h3>".$row['Title']."</h3>
-                  <p>".$row['Description']."</p>
-              </div>";
-            }
-          } else {
-            echo "There are no results matching your search!";
-          }
-        }
+        // if (isset($_POST['submit-search'])) {
+        //   $search = mysqli_real_escape_string($conn, $_POST['search']);
+        //   $sql = "SELECT * FROM products WHERE Title LIKE '%$search%' OR Description LIKE '%$search%'";
+        //   $result = mysqli_query($conn, $sql);
+        //   $queryResult = mysqli_num_rows($result);
+        //
+        //   if ($queryResult > 0) {
+        //     while ($row = mysqli_fetch_assoc($result)) {
+        //       echo "<div>
+        //           <img width='500' src=".$row['img_dir'].">
+        //           <h3>".$row['Title']."</h3>
+        //           <p>".$row['Description']."</p>
+        //       </div>";
+        //     }
+        //   } else {
+        //     echo "There are no results matching your search!";
+        //   }
+        // }
       ?>
     </div>
   </body>
